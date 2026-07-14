88586870838c1325b761d25812214ceff8b43b99
arretez95 <73412535+arretez95@users.noreply.github.com>
2026-07-13 18:27:56 -0700

    Fixed conditional logic, random * 3 instead of 2

--- main.js
+++ main.js
@@ -1,14 +1,14 @@
 console.log("Hello!")
 
 function getComputerChoice() {
-	return Math.floor(Math.random * 100) + 1
-	/* if (random === 2) {
+	let random = Math.floor(Math.random() * 3)
+	if (random === 2) {
 		return "rock"
 	} else if (random === 1) {
 		return "paper"
 	} else if (random === 0) {
 		return "scissors"
-	} */
+	}
 }
 
 console.log(getComputerChoice())
