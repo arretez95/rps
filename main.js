ef29a0a70d27d507eddbdebfd6e741d96d75f54e
arretez95 <73412535+arretez95@users.noreply.github.com>
2026-07-13 18:40:55 -0700

    Added logic for playiing paper & scissors

--- main.js
+++ main.js
@@ -36,5 +36,36 @@ function playRound(humanChoice, computerChoice) {
 				Your score: ${humanScore}
 				Computer score: ${computerScore}`
 			}
+			break
+		case "paper":
+			if (computerChoice === "rock") {
+				humanScore++
+				`You win!
+				Your score: ${humanScore}
+				Computer score: ${computerScore}`
+			} else if (computerChoice === "paper") {
+				"It's a draw!"
+			} else if (computerChoice === "scissors") {
+				computerScore++
+				`You lose!
+				Your score: ${humanScore}
+				Computer score: ${computerScore}`
+			}
+			break
+		case "scissors":
+			if (computerChoice === "rock") {
+				computerScore++
+				`You lose!
+				Your score: ${humanScore}
+				Computer score: ${computerScore}`
+			} else if (computerChoice === "paper") {
+				humanScore++
+				`You win!
+				Your score: ${humanScore}
+				Computer score: ${computerScore}`
+			} else if (computerChoice === "scissors") {
+				"It's a draw!"
+			}
+			break
 	}
 }
