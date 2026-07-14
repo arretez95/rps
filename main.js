547dab8c503bc0120faed3a540aa17544c69a48b
arretez95 <73412535+arretez95@users.noreply.github.com>
2026-07-13 18:22:16 -0700

    Added conditional logic to select RPS

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 console.log("Hello!")
 
 function getComputerChoice() {
-	return Math.floor(Math.random * 2)
+	return Math.floor(Math.random * 100) + 1
+	/* if (random === 2) {
+		return "rock"
+	} else if (random === 1) {
+		return "paper"
+	} else if (random === 0) {
+		return "scissors"
+	} */
 }
+
+console.log(getComputerChoice())
