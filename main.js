16389ab304b580bedfacc0c6c423e1959bbabd2d
arretez95 <73412535+arretez95@users.noreply.github.com>
2026-07-19 11:32:00 -0700

    Added text to show computer play.

--- main.js
+++ main.js
@@ -17,40 +17,44 @@ function getComputerChoice() {
 
 function playRound(humanChoice, computerChoice) {
     let player = humanChoice.toLowerCase()
+    divResult.textContent = `Computer chose ${computerChoice}. `
     switch (player) {
         case "rock":
             if (computerChoice === "rock") {
-                divResult.textContent = "It's a draw!"
+                divResult.textContent += "It's a draw!"
             } else if (computerChoice === "paper") {
                 computerScore++
-                divResult.textContent = "You lose!" 
+                divResult.textContent += "You lose!" 
             } else if (computerChoice === "scissors") {
                 humanScore++
-                divResult.textContent = "You win!"
+                divResult.textContent += "You win!"
             }
             break
         case "paper":
             if (computerChoice === "rock") {
                 humanScore++
-                divResult.textContent = "You win!" 
+                divResult.textContent += "You win!" 
             } else if (computerChoice === "paper") {
-                divResult.textContent = "It's a draw!"
+                divResult.textContent += "It's a draw!"
             } else if (computerChoice === "scissors") {
                 computerScore++
-                divResult.textContent = "You lose!"
+                divResult.textContent += "You lose!"
             }
             break
         case "scissors":
             if (computerChoice === "rock") {
                 computerScore++
-                divResult.textContent = "You lose!"
+                divResult.textContent += "You lose!"
             } else if (computerChoice === "paper") {
                 humanScore++
-                divresult.textContent = "You win!"
+                divresult.textContent += "You win!"
             } else if (computerChoice === "scissors") {
-                divResult.textContent = "It's a draw!"
+                divResult.textContent += "It's a draw!"
             }
             break
+    }
+    if (computerScore === 5 || humanScore === 5) {
+        divResult.textContent += computerScore > humanScore ? " Computer wins!" : " Player wins!"
     }
     divScore.textContent = `You: ${humanScore} | Computer: ${computerScore}`
 }
@@ -58,7 +62,10 @@ function playRound(humanChoice, computerChoice) {
     
     
 document.addEventListener("click", (e) => {
+    console.log(e.target.tagName)
     const humanSelection = e.target.id
     const computerSelection = getComputerChoice()
-    playRound(humanSelection, computerSelection)
+    if (e.target.tagName === "BUTTON") {
+        playRound(humanSelection, computerSelection)
+    }
 })
